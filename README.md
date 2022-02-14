f629573f1275ba7efafbeecbc3d6f29e2e8fa13e
Kosma Moczek <kosma@kosma.pl>
2022-02-14 04:04:10 +0100

    switch to three-step operation

--- README.md
+++ README.md
@@ -9,16 +9,26 @@ the flying machine based farm made by neffty87.
 You should only run this in disposable worlds, either in single player or on servers
 where you have op. The mod *will* destroy the immediate surroundings of the geode!
 
-Run `/geodesy <geode center coordinates> <primary axis> <secondary axis>`, e.g.
-`/geodesy ~ ~ ~ z x`. The entire area around you will be cleared, and two walls
-of blocks will be created. In each wall:
+1. Run `/geodesy prepare <first corner> <second corner>` to clear out the surrounding
+area of any junk and calculate the geode area. The area will be highlighted for you
+to verify you've entered correct coordinates.
 
-* Slime block means this location contains amethyst clusters to be pushed and destroyed.
-* Obsidian block means this location contains budding amethyst blocks, so the flying
-  machine must not fly through that location.
-* Crying obsidian is same as obsidian, except indicates a possible inefficiency due
-  to budding amethyst layout.
+You don't have to be exact with the corner coordinates - you can select a larger
+volume and the mod will find the geode anyway. The only time you have to be careful
+is when you have two geodes very close to each other (this is also the reason we don't
+have autodetection - it easily got confused with multiple geodes nearby).
 
-Building the required flying machines is left as an exercise for the reader. (:
+2. Run `/geodesy build <axes>` to create the obsidian structure:
+
+* Moss block means this location contains amethyst clusters to be pushed and destroyed.
+* Crying obsidian block means this location contains budding amethyst blocks, so
+  the flying machine must not fly through that location.
+* Obsidian marks the outer frame of the machine.
+
+3. Place the slime and honey blocks outside the structure, as shown by ilmango.
+
+4. Run `/geodesy collapse` to "push" the slime and honey blocks inside the obsidian frame.
+
+5. Build the flying machines, as shown by ilmango.
 
 ![screenshot](screenshot.png)
--- src/main/java/pl/kosma/geodesy/Geodesy.java
+++ src/main/java/pl/kosma/geodesy/Geodesy.java
@@ -8,6 +8,8 @@ import net.minecraft.block.AmethystClusterBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.StructureBlockBlockEntity;
+import net.minecraft.block.enums.StructureBlockMode;
 import net.minecraft.block.enums.WallMountLocation;
 import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.state.property.Properties;
@@ -28,6 +30,9 @@ import static net.minecraft.server.command.CommandManager.*;
 public class Geodesy implements ModInitializer {
     final int BUILD_MARGIN = 11;
     final int WALL_OFFSET = 2;
+
+    private World world;
+    private IterableBlockBox geode;
 
     private static class IterableBlockBox extends BlockBox {
 
@@ -72,19 +77,131 @@ public class Geodesy implements ModInitializer {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("geodesy")
                     .requires(source -> source.hasPermissionLevel(2))
-                    .then(argument("startPos", BlockPosArgumentType.blockPos())
+                    .then(literal("prepare")
+                        .then(argument("startPos", BlockPosArgumentType.blockPos())
                             .then(argument("endPos", BlockPosArgumentType.blockPos())
-                                    .then(argument("axes", StringArgumentType.string()).executes(context -> {
-                                            World world = context.getSource().getPlayer().getWorld();
-                                            BlockPos startPos = BlockPosArgumentType.getBlockPos(context, "startPos");
-                                            BlockPos endPos = BlockPosArgumentType.getBlockPos(context, "endPos");
-                                            String axes = StringArgumentType.getString(context, "axes");
-                                            Direction.Axis[] axisList = new Direction.Axis[axes.length()];
-                                            Arrays.setAll(axisList, i -> Direction.Axis.fromName(axes.substring(i, i+1)));
-                                            this.runGeodesy(world, startPos, endPos, axisList);
-                                            return Command.SINGLE_SUCCESS;
-                                        })
-            ))));
+                                .executes(context -> {
+                                    World world = context.getSource().getPlayer().getWorld();
+                                    BlockPos startPos = BlockPosArgumentType.getBlockPos(context, "startPos");
+                                    BlockPos endPos = BlockPosArgumentType.getBlockPos(context, "endPos");
+                                    this.geodesyPrepare(world, startPos, endPos);
+                                    return Command.SINGLE_SUCCESS;
+                                }))))
+                    .then(literal("build")
+                        .then(argument("axes", StringArgumentType.string())
+                            .executes(context -> {
+                                String axes = StringArgumentType.getString(context, "axes");
+                                Direction.Axis[] axisList = new Direction.Axis[axes.length()];
+                                Arrays.setAll(axisList, i -> Direction.Axis.fromName(axes.substring(i, i+1)));
+                                this.geodesyBuild(axisList);
+                                return Command.SINGLE_SUCCESS;
+                            })))
+                    .then(literal("collapse").executes(context -> {
+                        this.geodesyCollapse();
+                        return Command.SINGLE_SUCCESS;
+                    }))
+            );
+        });
+    }
+
+    private void geodesyPrepare(World world, BlockPos startPos, BlockPos endPos) {
+        this.world = world;
+
+        // Detect the geode area.
+        this.geode = detectGeode(world, startPos, endPos);
+
+        // Expand the area and clear it out for work purposes.
+        this.prepareWorkArea();
+        this.growClusters();
+
+        // Highlight the geode area.
+        int offset = WALL_OFFSET+1;
+        BlockPos structureBlockPos = new BlockPos(geode.getMinX()-offset, geode.getMinY()-offset, geode.getMinZ()-offset);
+        world.setBlockState(structureBlockPos, Blocks.STRUCTURE_BLOCK.getDefaultState(), NOTIFY_LISTENERS);
+        StructureBlockBlockEntity structure = (StructureBlockBlockEntity) world.getBlockEntity(structureBlockPos);
+        structure.setMode(StructureBlockMode.SAVE);
+        structure.setStructureName("geode");
+        structure.setOffset(new BlockPos(offset, offset, offset));
+        structure.setSize(geode.getDimensions().add(1, 1, 1));
+        structure.setShowBoundingBox(true);
+        structure.markDirty();
+    }
+
+    private void geodesyBuild(Direction.Axis[] axes) {
+        // Clear the working area.
+        this.prepareWorkArea();
+
+        // Generate grown crystals in the working area.
+        this.growClusters();
+
+        // Run the projection.
+        for (Direction.Axis axis: axes) {
+            this.projectGeode(axis);
+        }
+
+        // Add a frame.
+        IterableBlockBox frameBoundingBox = new IterableBlockBox(geode.expand(WALL_OFFSET));
+        frameBoundingBox.forEachPosition(blockPos -> {
+            // Add a wireframe of obsidian around the farm.
+            int count = 0;
+            if (blockPos.getX() == frameBoundingBox.getMinX() || blockPos.getX() == frameBoundingBox.getMaxX())
+                count++;
+            if (blockPos.getY() == frameBoundingBox.getMinY() || blockPos.getY() == frameBoundingBox.getMaxY())
+                count++;
+            if (blockPos.getZ() == frameBoundingBox.getMinZ() || blockPos.getZ() == frameBoundingBox.getMaxZ())
+                count++;
+            if (count >= 2) {
+                world.setBlockState(blockPos, Blocks.OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
+            } else {
+                // Opposite wall gets crying obsidian
+                if (blockPos.getX() == frameBoundingBox.getMinX() ||
+                        blockPos.getY() == frameBoundingBox.getMinY() ||
+                        blockPos.getZ() == frameBoundingBox.getMinZ()) {
+                    world.setBlockState(blockPos, Blocks.CRYING_OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
+                }
+            }
+        });
+
+        // Replace all remaining amethyst clusters with buttons so items can't
+        // fall on them and get stuck.
+        frameBoundingBox.forEachPosition(blockPos -> {
+            BlockState blockState = world.getBlockState(blockPos);
+            if (blockState.getBlock() == Blocks.AMETHYST_CLUSTER) {
+                BlockState button = Blocks.POLISHED_BLACKSTONE_BUTTON.getDefaultState();
+                Direction facing = blockState.get(Properties.FACING);
+                button = switch (facing) {
+                    case DOWN -> button.with(Properties.WALL_MOUNT_LOCATION, WallMountLocation.CEILING);
+                    case UP -> button.with(Properties.WALL_MOUNT_LOCATION, WallMountLocation.FLOOR);
+                    default -> button.with(Properties.HORIZONTAL_FACING, facing);
+                };
+                world.setBlockState(blockPos, button, NOTIFY_LISTENERS);
+            }
+        });
+    }
+
+    private void geodesyCollapse() {
+        // Run along all the axes and move all slime/honey blocks inside the frame.
+        for (Direction.Axis axis: Direction.Axis.values()) {
+            geode.slice(axis, slice -> {
+                // Calculate positions of the source and target blocks for moving.
+                BlockPos targetPos = new BlockPos(slice.getMaxX(), slice.getMaxY(), slice.getMaxZ()).offset(axis, WALL_OFFSET);
+                BlockPos sourcePos = targetPos.offset(axis, 1);
+                Block sourceBlock = world.getBlockState(sourcePos).getBlock();
+                Block targetBlock = world.getBlockState(targetPos).getBlock();
+                // Check that the operation can succeed.
+                if (sourceBlock != Blocks.AIR && targetBlock != Blocks.CRYING_OBSIDIAN) {
+                    world.setBlockState(targetPos, world.getBlockState(sourcePos), NOTIFY_LISTENERS);
+                    world.setBlockState(sourcePos, Blocks.AIR.getDefaultState(), NOTIFY_LISTENERS);
+                }
+            });
+        }
+    }
+
+    private void prepareWorkArea() {
+        IterableBlockBox workBoundingBox = new IterableBlockBox(geode.expand(BUILD_MARGIN));
+        workBoundingBox.forEachPosition(blockPos -> {
+            if (world.getBlockState(blockPos).getBlock() != Blocks.BUDDING_AMETHYST)
+                world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), NOTIFY_LISTENERS);
         });
     }
 
@@ -110,8 +227,8 @@ public class Geodesy implements ModInitializer {
         return new IterableBlockBox(BlockBox.encompassPositions(amethystPositions).orElse(null).expand(1));
     }
 
-    private void growClusters(World world, IterableBlockBox geodeBoundingBox) {
-        geodeBoundingBox.forEachPosition(blockPos -> {
+    private void growClusters() {
+        geode.forEachPosition(blockPos -> {
             if (world.getBlockState(blockPos).getBlock() == Blocks.BUDDING_AMETHYST) {
                 for (Direction direction: Direction.values()) {
                     BlockPos budPos = blockPos.offset(direction);
@@ -122,8 +239,8 @@ public class Geodesy implements ModInitializer {
         });
     }
 
-    private void projectGeode(World world, IterableBlockBox geodeBoundingBox, Direction.Axis sliceAxis) {
-        geodeBoundingBox.slice(sliceAxis, slice -> {
+    private void projectGeode(Direction.Axis axis) {
+        geode.slice(axis, slice -> {
             // For each slice, determine the block composition
             AtomicBoolean hasBlock = new AtomicBoolean(false);
             AtomicBoolean hasCluster = new AtomicBoolean(false);
@@ -151,63 +268,8 @@ public class Geodesy implements ModInitializer {
                 });
             }
             // Set sidewall block
-            BlockPos wallPos = new BlockPos(slice.getMaxX(), slice.getMaxY(), slice.getMaxZ()).offset(sliceAxis, this.WALL_OFFSET);
+            BlockPos wallPos = new BlockPos(slice.getMaxX(), slice.getMaxY(), slice.getMaxZ()).offset(axis, this.WALL_OFFSET);
             world.setBlockState(wallPos, wallBlock.getDefaultState());
-        });
-    }
-
-    private void runGeodesy(World world, BlockPos startPos, BlockPos endPos, Direction.Axis[] axes) {
-        IterableBlockBox geodeBoundingBox = detectGeode(world, startPos, endPos);
-
-        // Expand the area and clear it out for work purposes.
-        IterableBlockBox workBoundingBox = new IterableBlockBox(geodeBoundingBox.expand(BUILD_MARGIN));
-        workBoundingBox.forEachPosition(blockPos -> {
-            if (world.getBlockState(blockPos).getBlock() != Blocks.BUDDING_AMETHYST)
-                world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), NOTIFY_LISTENERS);
-        });
-
-        // Generate grown crystals in the working area.
-        growClusters(world, geodeBoundingBox);
-
-        // Run the projection.
-        for (Direction.Axis axis: axes) {
-            this.projectGeode(world, geodeBoundingBox, axis);
-        }
-
-        // Postprocess the area.
-        IterableBlockBox frameBoundingBox = new IterableBlockBox(geodeBoundingBox.expand(WALL_OFFSET));
-        frameBoundingBox.forEachPosition(blockPos -> {
-            // Add a wireframe of obsidian around the farm.
-            int count = 0;
-            if (blockPos.getX() == frameBoundingBox.getMinX() || blockPos.getX() == frameBoundingBox.getMaxX())
-                count++;
-            if (blockPos.getY() == frameBoundingBox.getMinY() || blockPos.getY() == frameBoundingBox.getMaxY())
-                count++;
-            if (blockPos.getZ() == frameBoundingBox.getMinZ() || blockPos.getZ() == frameBoundingBox.getMaxZ())
-                count++;
-            if (count >= 2) {
-                world.setBlockState(blockPos, Blocks.OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
-            } else {
-                // Opposite wall gets crying obsidian
-                if (blockPos.getX() == frameBoundingBox.getMinX() ||
-                    blockPos.getY() == frameBoundingBox.getMinY() ||
-                    blockPos.getZ() == frameBoundingBox.getMinZ()) {
-                    world.setBlockState(blockPos, Blocks.CRYING_OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
-                }
-            }
-            // Replace all remaining amethyst clusters with buttons so items can't
-            // fall on them and get stuck.
-            BlockState blockState = world.getBlockState(blockPos);
-            if (blockState.getBlock() == Blocks.AMETHYST_CLUSTER) {
-                BlockState button = Blocks.POLISHED_BLACKSTONE_BUTTON.getDefaultState();
-                Direction facing = blockState.get(Properties.FACING);
-                button = switch (facing) {
-                    case DOWN -> button.with(Properties.WALL_MOUNT_LOCATION, WallMountLocation.CEILING);
-                    case UP -> button.with(Properties.WALL_MOUNT_LOCATION, WallMountLocation.FLOOR);
-                    default -> button.with(Properties.HORIZONTAL_FACING, facing);
-                };
-                world.setBlockState(blockPos, button, NOTIFY_LISTENERS);
-            }
         });
     }
 }
