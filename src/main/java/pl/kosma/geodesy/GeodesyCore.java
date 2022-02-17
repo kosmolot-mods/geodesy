9afb4c6c17cc780944068e12223db0e00b991573
Kosma Moczek <kosma@kosma.pl>
2022-02-17 03:35:19 +0100

    use crying obsidian for rear machine blocker, nice light source it is

--- src/main/java/pl/kosma/geodesy/GeodesyCore.java
+++ src/main/java/pl/kosma/geodesy/GeodesyCore.java
@@ -403,7 +403,7 @@ public class GeodesyCore {
         world.setBlockState(pos.offset(directionUp, 1), Blocks.OBSERVER.getDefaultState().with(Properties.FACING, directionAlong), NOTIFY_LISTENERS);
         pos = pos.offset(directionAlong, 2);
         // [SKIP AGAIN!] Eighth layer: blocker
-        world.setBlockState(pos.offset(directionUp, 0), Blocks.OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
+        world.setBlockState(pos.offset(directionUp, 0), Blocks.CRYING_OBSIDIAN.getDefaultState(), NOTIFY_LISTENERS);
     }
 
     /*
